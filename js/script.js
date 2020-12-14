4a848c6fbdca33b18112ac4dcf14a51dcf643a60
sparshoza <sparsh.oza@colorkrew.com>
2020-12-14 15:38:51 +0900

    Merge branch 'master' of github.com:sparshoza/showdown

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function run() {
-  var text = document.getElementById('sourceTextArea').value,
-      target = document.getElementById('targetDiv'),
-      converter = new showdown.Converter(),
-      html = converter.makeHtml(text);
-    
-    target.innerHTML = html;
-}
